7bbbc6b66ad61b0b3f7dc03deaf597c6291daae8
JM0KA <117128993+JM0KA@users.noreply.github.com>
2026-06-11 15:23:46 -0600

    Upon setting up android and web app version of this application I hid the firebase configuration credentials using flutter_dotenv to prevent exposing keys in source control and added the .env file into the .gitignore along side the googleService file

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
 import 'package:instagram_clone/utils/colours.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
-void main() {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(apiKey: dotenv.env['API_KEY'] ?? '', appId: dotenv.env['APP_ID'] ?? '', messagingSenderId: dotenv.env['MSG_SENDER_ID'] ?? '', projectId: dotenv.env['PROJECT_ID'] ?? '', storageBucket: dotenv.env['STORAGE_BUCKET'] ?? ''),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   runApp(const MyApp());
 }
 
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,3 +1,2 @@
-import 'package:flutter/material.dart';
 
 const webScreenLayout = 600;
