e9b6772c34bcbb2736f9e2f5dd9338de5140e3d1
JM0KA <117128993+JM0KA@users.noreply.github.com>
2026-06-22 17:11:21 -0600

    Rerstructered some files and ensured the application was able to run properly after running into some build errors.

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:instagram_clone/screens/login_screen.dart';
 import 'package:instagram_clone/utils/colours.dart';
 import 'package:instagram_clone/responsive/mobile_screen_layout.dart';
 import 'package:instagram_clone/responsive/web_screen_layout.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
 
-  if (kIsWeb) {
-    await Firebase.initializeApp(
-      options: FirebaseOptions(apiKey: dotenv.env['API_KEY'] ?? '', appId: dotenv.env['APP_ID'] ?? '', messagingSenderId: dotenv.env['MSG_SENDER_ID'] ?? '', projectId: dotenv.env['PROJECT_ID'] ?? '', storageBucket: dotenv.env['STORAGE_BUCKET'] ?? ''),
-    );
-  } else {
-    await Firebase.initializeApp();
-  }
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
@@ -32,7 +26,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Instagram Clone',
       theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: ResponsiveLayout(webScreenlayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout()),
+      // home: ResponsiveLayout(webScreenlayout: WebScreenLayout(), mobileScreenLayout: MobileScreenLayout()),
+      home: LoginScreen(),
     );
   }
 }
